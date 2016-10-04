4f5260aa08542032ba68d4361ac0560da180f7e1
Michael Christy <emonomics@gmail.com>
2016-10-03 20:11:39 -0400

    initial push on challenge 6

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -4,6 +4,14 @@
 		
 //Code Challenge:
 //Create an object named "Box" with 3 properties, height, width, volume.
+var height = 8px;
+var width = 8px;
+var volume = (height * width);
+
+$('#Print').click(function box(height, width, volume){
+    $('body').append('.output');
+    console.log('ive been clicked');
+})
 //Create 2 buttons for Height. The first button decreases the Box Height by 1. The second button increases the Box Height by 1.
 //Create a button that prints the object and its attributes to the page (use the span "output".
  
