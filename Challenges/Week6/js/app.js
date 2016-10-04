3e49d0d76ffe3017f0cd35da695d6e90cd1cdeec
Michael Christy <emonomics@gmail.com>
2016-10-03 21:33:47 -0400

    set array to increment values for height, weight, and volume

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -1,18 +1,45 @@
 //#Week 6 JQuery Code Challenge
 
 //Objects
-		
+var box = {
+    "height" : 1,
+    "weight" : 1,
+    "volume" : 1
+};
+
 //Code Challenge:
 //Create an object named "Box" with 3 properties, height, width, volume.
-var height = 8px;
-var width = 8px;
-var volume = (height * width);
 
-$('#Print').click(function box(height, width, volume){
-    $('body').append('.output');
+$('#Print').click(function() {
+    $('#output').append();
     console.log('ive been clicked');
-})
+});
+
+$('#HeightDecrease').click(function(){
+    box.height--;
+});
+
+$('#HeightIncrease').click(function(){
+     box.height++;
+});
+
+$('#WeightDecrease').click(function(){
+    box.weight--;
+});
+
+$('#WeightIncrease').click(function(){
+    box.weight++;
+});
+
+$('#VolumeDecrease').click(function(){
+   box.volume--;
+});
+
+$('#VolumeIncrease').click(function(){
+    box.volume++;
+});
 //Create 2 buttons for Height. The first button decreases the Box Height by 1. The second button increases the Box Height by 1.
+
 //Create a button that prints the object and its attributes to the page (use the span "output".
  
 //Extra credit
