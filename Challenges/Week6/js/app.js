383f64d6506f1b01d631450c9a459a5eae8b15a6
Michael Christy <emonomics@gmail.com>
2016-10-03 21:57:48 -0400

    clicking the box button will now post sizes to the span

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -1,46 +1,44 @@
 //#Week 6 JQuery Code Challenge
 
 //Objects
+//Create an object named "Box" with 3 properties, height, width, volume.
 var box = {
-    "height" : 1,
-    "weight" : 1,
-    "volume" : 1
+    "height": 1,
+    "weight": 1,
+    "volume": 1
 };
 
-//Code Challenge:
-//Create an object named "Box" with 3 properties, height, width, volume.
 
-$('#Print').click(function() {
-    $('#output').append();
-    console.log('ive been clicked');
-});
-
-$('#HeightDecrease').click(function(){
+//Create 2 buttons for Height. The first button decreases the Box Height by 1. The second button increases the Box Height by 1.
+$('#HeightDecrease').click(function () {
     box.height--;
 });
 
-$('#HeightIncrease').click(function(){
-     box.height++;
+$('#HeightIncrease').click(function () {
+    box.height++;
 });
 
-$('#WeightDecrease').click(function(){
+$('#WeightDecrease').click(function () {
     box.weight--;
 });
 
-$('#WeightIncrease').click(function(){
+$('#WeightIncrease').click(function () {
     box.weight++;
 });
 
-$('#VolumeDecrease').click(function(){
-   box.volume--;
+$('#VolumeDecrease').click(function () {
+    box.volume--;
 });
 
-$('#VolumeIncrease').click(function(){
+$('#VolumeIncrease').click(function () {
     box.volume++;
 });
-//Create 2 buttons for Height. The first button decreases the Box Height by 1. The second button increases the Box Height by 1.
 
 //Create a button that prints the object and its attributes to the page (use the span "output".
- 
+$('#Print').click(function () {
+    $('#output').html("<p>Height: " + box.height + "</P></br><p>Weight: " + box.weight + "</P></br><p>Volume: " + box.volume + "</P>");
+});
+
+
 //Extra credit
 //Create interactive buttons to decrease or increase the Width and Volume
